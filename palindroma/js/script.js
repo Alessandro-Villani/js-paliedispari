d170c0518ee57c79df6553ea7eb82b5e395a9efe
Alessandro-Villani <alessvill@libero.it>
2022-12-15 15:41:22 +0100

    added target and listener

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -4,3 +4,23 @@ Chiedere all’utente di inserire una parola tramiite un form
 Creare una funzione per capire se la parola inserita è palindroma
 
 ----------------------------------------------------------------------------------------*/
+
+//1. Select input from DOM
+
+const inputWord = document.getElementById('word');
+
+//2. Select form from DOM
+
+const inputForm = document.querySelector('form');
+console.log(inputForm);
+
+//3. Select output from DOM
+
+const outputTarget = document.getElementById('target');
+
+//4. Add event listener
+
+inputForm.addEventListener('click', function(event){
+    event.preventDefault();
+    
+})
